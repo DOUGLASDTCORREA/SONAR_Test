fc6f2ba08bcf5d3104a96aa2474e30f8da463843
DOUGLASDTCORREA <douglas.correa@ultracargo.com.br>
2025-05-28 11:47:51 -0300

    Create DecisionRoom.tsx

---
+++ src/pages/DecisionRoom.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function DecisionRoom() {
+  return (
+    <div>
+      <h2>Sala de Decisão</h2>
+      <p>
+        Seção exclusiva para decisores, com relatórios, análises setoriais, estudos e ferramentas estratégicas.<br />
+        Em breve, conteúdos restritos para gerentes, diretores e C-level estarão disponíveis aqui.
+      </p>
+    </div>
+  );
+}
