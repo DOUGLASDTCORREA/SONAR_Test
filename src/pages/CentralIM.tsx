b3180451a9f9401534e21fc02a0425d6d6bc41c7
DOUGLASDTCORREA <douglas.correa@ultracargo.com.br>
2025-05-28 11:48:13 -0300

    Create CentralIM.tsx

---
+++ src/pages/CentralIM.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function CentralIM() {
+  return (
+    <div>
+      <h2>Central IM</h2>
+      <p>
+        Área exclusiva para editores, com acesso a todos os relatórios, estudos, ferramentas e dashboards.<br />
+        Em breve, conteúdos completos para publicação e gestão estarão disponíveis aqui.
+      </p>
+    </div>
+  );
+}
