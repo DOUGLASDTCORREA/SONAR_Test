f9e511e1f2a967399b920f9d8aaa1aa26c4c0dd8
DOUGLASDTCORREA <douglas.correa@ultracargo.com.br>
2025-05-28 12:13:42 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, NavLink } from 'react-router-dom';
+import { HashRouter as Router, NavLink } from 'react-router-dom';
 import Routes from './routes';
 import './assets/styles/theme.css';
 
