e17857d88bb9bb9ea6e590b166cd510afcfedcd8
DOUGLASDTCORREA <douglas.correa@ultracargo.com.br>
2025-05-28 11:45:54 -0300

    Create Foresight.tsx

---
+++ src/pages/Foresight.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Foresight() {
+  return (
+    <div>
+      <h2>Foresight</h2>
+      <p>
+        Seção dedicada a análises prospectivas, tendências de mercado, sinais fracos, cenários futuros e insights estratégicos.<br />
+        Em breve, conteúdos exclusivos para estrategistas estarão disponíveis aqui.
+      </p>
+    </div>
+  );
+}
